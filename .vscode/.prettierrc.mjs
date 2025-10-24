72538e63c1748281dd89cca4cf13c0af2364615f
Amy Liu <mialana@users.noreply.github.com>
2025-10-24 17:31:00 -0400

    ci: update .gitignore

---
+++ .vscode/.prettierrc.mjs
@@ -0,0 +1,11 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+
+export default {
+    // Global default options for all files
+    printWidth: 80,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    endOfLine: "auto",
+};
