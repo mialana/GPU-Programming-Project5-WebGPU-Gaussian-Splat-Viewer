55f1a61cd82b392872c9600b40696c0b7be3b2d2
Amy Liu <mialana@users.noreply.github.com>
2025-10-24 17:24:28 -0400

    ci: run prettier and standardize eol in VSCode settings

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,34 @@
-import './style.css'
-import init from './renderers/renderer';
-import { assert } from './utils/util';
+import "./style.css";
+import init from "./renderers/renderer";
+import { assert } from "./utils/util";
 
 (async () => {
-  
-  if (navigator.gpu === undefined) {
-    const h = document.querySelector('#title') as HTMLElement;
-    h.innerText = 'WebGPU is not supported in this browser.';
-    return;
-  }
-  const adapter = await navigator.gpu.requestAdapter({
-    powerPreference: 'high-performance',
-  });
-  if (adapter === null) {
-    const h = document.querySelector('#title') as HTMLElement;
-    h.innerText = 'No adapter is available for WebGPU.';
-    return;
-  }
-  
-  const device = await adapter.requestDevice({
-    requiredLimits: { 
-      maxComputeWorkgroupStorageSize: adapter.limits.maxComputeWorkgroupStorageSize,
-      maxStorageBufferBindingSize: adapter.limits. maxStorageBufferBindingSize
-     },
-  });
+    if (navigator.gpu === undefined) {
+        const h = document.querySelector("#title") as HTMLElement;
+        h.innerText = "WebGPU is not supported in this browser.";
+        return;
+    }
+    const adapter = await navigator.gpu.requestAdapter({
+        powerPreference: "high-performance",
+    });
+    if (adapter === null) {
+        const h = document.querySelector("#title") as HTMLElement;
+        h.innerText = "No adapter is available for WebGPU.";
+        return;
+    }
 
-  const canvas = document.querySelector<HTMLCanvasElement>('#webgpu-canvas');
-  assert(canvas !== null);
-  const context = canvas.getContext('webgpu') as GPUCanvasContext;
-  
-  init(canvas, context, device);
-  
+    const device = await adapter.requestDevice({
+        requiredLimits: {
+            maxComputeWorkgroupStorageSize:
+                adapter.limits.maxComputeWorkgroupStorageSize,
+            maxStorageBufferBindingSize:
+                adapter.limits.maxStorageBufferBindingSize,
+        },
+    });
+
+    const canvas = document.querySelector<HTMLCanvasElement>("#webgpu-canvas");
+    assert(canvas !== null);
+    const context = canvas.getContext("webgpu") as GPUCanvasContext;
+
+    init(canvas, context, device);
 })();
--- src/renderers/gaussian-renderer.ts
+++ src/renderers/gaussian-renderer.ts
@@ -1,86 +1,89 @@
-import { PointCloud } from '../utils/load';
-import preprocessWGSL from '../shaders/preprocess.wgsl';
-import renderWGSL from '../shaders/gaussian.wgsl';
-import { get_sorter,c_histogram_block_rows,C } from '../sort/sort';
-import { Renderer } from './renderer';
+import { PointCloud } from "../utils/load";
+import preprocessWGSL from "../shaders/preprocess.wgsl";
+import renderWGSL from "../shaders/gaussian.wgsl";
+import { get_sorter, c_histogram_block_rows, C } from "../sort/sort";
+import { Renderer } from "./renderer";
 
-export interface GaussianRenderer extends Renderer {
-
-}
+export interface GaussianRenderer extends Renderer {}
 
 // Utility to create GPU buffers
 const createBuffer = (
-  device: GPUDevice,
-  label: string,
-  size: number,
-  usage: GPUBufferUsageFlags,
-  data?: ArrayBuffer | ArrayBufferView
+    device: GPUDevice,
+    label: string,
+    size: number,
+    usage: GPUBufferUsageFlags,
+    data?: ArrayBuffer | ArrayBufferView,
 ) => {
-  const buffer = device.createBuffer({ label, size, usage });
-  if (data) device.queue.writeBuffer(buffer, 0, data);
-  return buffer;
+    const buffer = device.createBuffer({ label, size, usage });
+    if (data) device.queue.writeBuffer(buffer, 0, data);
+    return buffer;
 };
 
 export default function get_renderer(
-  pc: PointCloud,
-  device: GPUDevice,
-  presentation_format: GPUTextureFormat,
-  camera_buffer: GPUBuffer,
+    pc: PointCloud,
+    device: GPUDevice,
+    presentation_format: GPUTextureFormat,
+    camera_buffer: GPUBuffer,
 ): GaussianRenderer {
+    const sorter = get_sorter(pc.num_points, device);
 
-  const sorter = get_sorter(pc.num_points, device);
-  
-  // ===============================================
-  //            Initialize GPU Buffers
-  // ===============================================
+    // ===============================================
+    //            Initialize GPU Buffers
+    // ===============================================
 
-  const nulling_data = new Uint32Array([0]);
+    const nulling_data = new Uint32Array([0]);
 
-  // ===============================================
-  //    Create Compute Pipeline and Bind Groups
-  // ===============================================
-  const preprocess_pipeline = device.createComputePipeline({
-    label: 'preprocess',
-    layout: 'auto',
-    compute: {
-      module: device.createShaderModule({ code: preprocessWGSL }),
-      entryPoint: 'preprocess',
-      constants: {
-        workgroupSize: C.histogram_wg_size,
-        sortKeyPerThread: c_histogram_block_rows,
-      },
-    },
-  });
+    // ===============================================
+    //    Create Compute Pipeline and Bind Groups
+    // ===============================================
+    const preprocess_pipeline = device.createComputePipeline({
+        label: "preprocess",
+        layout: "auto",
+        compute: {
+            module: device.createShaderModule({ code: preprocessWGSL }),
+            entryPoint: "preprocess",
+            constants: {
+                workgroupSize: C.histogram_wg_size,
+                sortKeyPerThread: c_histogram_block_rows,
+            },
+        },
+    });
 
-  const sort_bind_group = device.createBindGroup({
-    label: 'sort',
-    layout: preprocess_pipeline.getBindGroupLayout(2),
-    entries: [
-      { binding: 0, resource: { buffer: sorter.sort_info_buffer } },
-      { binding: 1, resource: { buffer: sorter.ping_pong[0].sort_depths_buffer } },
-      { binding: 2, resource: { buffer: sorter.ping_pong[0].sort_indices_buffer } },
-      { binding: 3, resource: { buffer: sorter.sort_dispatch_indirect_buffer } },
-    ],
-  });
+    const sort_bind_group = device.createBindGroup({
+        label: "sort",
+        layout: preprocess_pipeline.getBindGroupLayout(2),
+        entries: [
+            { binding: 0, resource: { buffer: sorter.sort_info_buffer } },
+            {
+                binding: 1,
+                resource: { buffer: sorter.ping_pong[0].sort_depths_buffer },
+            },
+            {
+                binding: 2,
+                resource: { buffer: sorter.ping_pong[0].sort_indices_buffer },
+            },
+            {
+                binding: 3,
+                resource: { buffer: sorter.sort_dispatch_indirect_buffer },
+            },
+        ],
+    });
 
+    // ===============================================
+    //    Create Render Pipeline and Bind Groups
+    // ===============================================
 
-  // ===============================================
-  //    Create Render Pipeline and Bind Groups
-  // ===============================================
-  
+    // ===============================================
+    //    Command Encoder Functions
+    // ===============================================
 
-  // ===============================================
-  //    Command Encoder Functions
-  // ===============================================
-  
-
-  // ===============================================
-  //    Return Render Object
-  // ===============================================
-  return {
-    frame: (encoder: GPUCommandEncoder, texture_view: GPUTextureView) => {
-      sorter.sort(encoder);
-    },
-    camera_buffer,
-  };
+    // ===============================================
+    //    Return Render Object
+    // ===============================================
+    return {
+        frame: (encoder: GPUCommandEncoder, texture_view: GPUTextureView) => {
+            sorter.sort(encoder);
+        },
+        camera_buffer,
+    };
 }
